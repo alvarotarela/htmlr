f756117b8abb74041eba0ffc3705142726d71cd5
usuario <alvarotarela@gmail.com>
2015-12-15 16:38:08 +0100

    martes 15

--- js/26-drag.js
+++ js/26-drag.js
@@ -23,30 +23,29 @@ $(document).ready(function(){
 
 			$('#botonmove').css('top',nuevaY);
 			$('#botonmove').css('left',nuevaX);
-			console.log('Movido a: ' + nuevaX + ', ' + nuevaY);
+			//console.log('Movido a: ' + nuevaX + ', ' + nuevaY);
 		}
 	});
 	
 
+	var lienzo = document.getElementById('lienzo');
+	var miLienzo1= lienzo.getContext('2d');
+	
 	$('#lienzo').on('touchmove',function(dedos){
-		var lienzo = document.getElementById('lienzo');
-		var miLienzo1= lienzo.getContext('2d');
+		
 		if (dedos.originalEvent.changedTouches.length > 0) {
 			var dedo = dedos.originalEvent.changedTouches[0];
 			var nuevaX = dedo.clientX;
 			var nuevaY = dedo.clientY;
-			console.log('POS: ' + nuevaX + ', ' + nuevaY);
+			//console.log('POS: ' + nuevaX + ', ' + nuevaY);
 			miLienzo1.fillStyle = "Blue";
 			miLienzo1.beginPath();
 			miLienzo1.moveTo(nuevaX,nuevaY);
 			miLienzo1.arc(nuevaX,nuevaY,5,0,360);
-			miLienzo1.closePath();
 			miLienzo1.fill();
+			miLienzo1.closePath();
 		}
 	});
-
-
-
 
 
 });
